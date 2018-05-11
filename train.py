a3565a35a80923875064f6e9bf54a2b3b8b7f899
Kai <3130000495@zju.edu.cn>
2018-05-11 14:07:25 -0400

    add loss function part

--- train.py
+++ train.py
@@ -3,9 +3,12 @@ import torch
 import datetime
 import argparse
 import numpy as np
-from scripts.utils import *
-from scripts.model.sqlnet import SQLNet
-
+from utils import *
+# from model.sqlnet import SQLNet
+from word_embedding import WordEmbedding
+from modules import MultiSqlPredictor,KeyWordPredictor,ColPredictor,OpPredictor,RootTeminalPredictor,DesAscLimitPredictor,AggPredictor
+TRAIN_COMPONENTS = ('multi_sql','keyword','col','op','agg','root_tem','des_asc')
+SQL_TOK = ['<UNK>', '<END>', 'WHERE', 'AND', 'EQL', 'GT', 'LT', '<BEG>']
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--toy', action='store_true',
@@ -18,11 +21,15 @@ if __name__ == '__main__':
             help='0: original dataset, 1: re-split dataset, 2: new complex dataset')
     parser.add_argument('--train_emb', action='store_true',
             help='Train word embedding.')
+    parser.add_argument('--train_component',type=str,default='',
+                        help='set train components,available:[multi_sql,keyword,col,op,agg,root_tem,des_asc]')
 
     args = parser.parse_args()
 
     N_word=300
     B_word=42
+    N_h = 200
+    N_depth=2
     if args.toy:
         USE_SMALL=True
         GPU=True
@@ -31,56 +38,75 @@ if __name__ == '__main__':
         USE_SMALL=False
         GPU=True
         BATCH_SIZE=64
-    TRAIN_ENTRY=(False, True, False)  # (AGG, SEL, COND)
-    TRAIN_AGG, TRAIN_SEL, TRAIN_COND = TRAIN_ENTRY
+    # TRAIN_ENTRY=(False, True, False)  # (AGG, SEL, COND)
+    # TRAIN_AGG, TRAIN_SEL, TRAIN_COND = TRAIN_ENTRY
     learning_rate = 1e-4
-
-    sql_data, table_data, val_sql_data, val_table_data, \
-            test_sql_data, test_table_data, \
-            TRAIN_DB, DEV_DB, TEST_DB = load_dataset(args.dataset, use_small=USE_SMALL)
+    if args.train_component not in TRAIN_COMPONENTS:
+        print("Invalid train component")
+        exit(1)
+    train_data = load_train_dataset(args.train_component)
+    # sql_data, table_data, val_sql_data, val_table_data, \
+    #         test_sql_data, test_table_data, \
+    #         TRAIN_DB, DEV_DB, TEST_DB = load_dataset(args.dataset, use_small=USE_SMALL)
 
     word_emb = load_word_emb('glove/glove.%dB.%dd.txt'%(B_word,N_word), \
             load_used=args.train_emb, use_small=USE_SMALL)
     #word_emb = load_concat_wemb('glove/glove.42B.300d.txt', "/data/projects/paraphrase/generation/para-nmt-50m/data/paragram_sl999_czeng.txt")
-
-    model = SQLNet(word_emb, N_word=N_word, gpu=GPU, trainable_emb=args.train_emb)
+    model = None
+    if args.train_component == "multi_sql":
+        model = MultiSqlPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    elif args.train_component == "keyword":
+        model = KeyWordPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    elif args.train_component == "col":
+        model = ColPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    elif args.train_component == "op":
+        model = OpPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    elif args.train_component == "agg":
+        model = AggPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    elif args.train_component == "root_tem":
+        model = RootTeminalPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    elif args.train_component == "des_asc":
+        model = DesAscLimitPredictor(N_word=N_word,N_h=N_h,N_depth=N_depth,gpu=GPU)
+    # model = SQLNet(word_emb, N_word=N_word, gpu=GPU, trainable_emb=args.train_emb)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay = 0)
 
-    agg_m, sel_m, cond_m = best_model_name(args)
-
-    if args.train_emb: # Load pretrained model.
-        agg_lm, sel_lm, cond_lm = best_model_name(args, for_load=True)
-        print "Loading from %s"%agg_lm
-        model.agg_pred.load_state_dict(torch.load(agg_lm))
-        print "Loading from %s"%sel_lm
-        model.selcond_pred.load_state_dict(torch.load(sel_lm))
-        print "Loading from %s"%cond_lm
-        model.cond_pred.load_state_dict(torch.load(cond_lm))
+    # agg_m, sel_m, cond_m = best_model_name(args)
+    #
+    # if args.train_emb: # Load pretrained model.
+    #     agg_lm, sel_lm, cond_lm = best_model_name(args, for_load=True)
+    #     print "Loading from %s"%agg_lm
+    #     model.agg_pred.load_state_dict(torch.load(agg_lm))
+    #     print "Loading from %s"%sel_lm
+    #     model.selcond_pred.load_state_dict(torch.load(sel_lm))
+    #     print "Loading from %s"%cond_lm
+    #     model.cond_pred.load_state_dict(torch.load(cond_lm))
 
 
     #initial accuracy
-    init_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, TRAIN_ENTRY)
-    if TRAIN_AGG:
-        torch.save(model.agg_pred.state_dict(), agg_m)
-    if TRAIN_SEL:
-        torch.save(model.selcond_pred.state_dict(), sel_m)
-    if TRAIN_COND:
-        torch.save(model.op_str_pred.state_dict(), cond_m)
+    # init_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, TRAIN_ENTRY)
+    # if TRAIN_AGG:
+    #     torch.save(model.agg_pred.state_dict(), agg_m)
+    # if TRAIN_SEL:
+    #     torch.save(model.selcond_pred.state_dict(), sel_m)
+    # if TRAIN_COND:
+    #     torch.save(model.op_str_pred.state_dict(), cond_m)
 
     print_flag = False
+    embed_layer = WordEmbedding(word_emb, N_word, gpu=GPU,
+                                SQL_TOK, trainable=args.train_emb)
+
     for i in range(500):
         print 'Epoch %d @ %s'%(i+1, datetime.datetime.now())
         print ' Loss = %s'%epoch_train(
-                model, optimizer, BATCH_SIZE,
-                sql_data, table_data, TRAIN_ENTRY)
-        train_tot_acc, train_bkd_acc = epoch_acc(model, BATCH_SIZE, sql_data, table_data, TRAIN_ENTRY)
-        print '\nTrain sel acc: %s, sel # acc: %s' % (train_bkd_acc[1], train_bkd_acc[0])
+                model, optimizer, BATCH_SIZE,embed_layer,train_data)
+        # train_tot_acc, train_bkd_acc = epoch_acc(model, BATCH_SIZE, sql_data, table_data, TRAIN_ENTRY)
+        # print '\nTrain sel acc: %s, sel # acc: %s' % (train_bkd_acc[1], train_bkd_acc[0])
         #print ' Breakdown results: agg #: %s, agg: %s, sel: %s, cond: %s, sel #: %s, cond #: %s, cond col: %s, cond op: %s, cond val: %s, group #: %s, group: %s, order #: %s, order: %s, order agg: %s, order par: %s'\
         #    % (train_bkd_acc[0], train_bkd_acc[1], train_bkd_acc[2], train_bkd_acc[3], train_bkd_acc[4], train_bkd_acc[5], train_bkd_acc[6], train_bkd_acc[7], train_bkd_acc[8], train_bkd_acc[9], train_bkd_acc[10], train_bkd_acc[11], train_bkd_acc[12], train_bkd_acc[13], train_bkd_acc[14])
-        if i > 497:
-            print_flag = True
-        val_tot_acc, val_bkd_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, TRAIN_ENTRY, error_print = print_flag, train_flag = False) #for detailed error analysis, pass True to error_print
-        print '\nDev sel acc: %s, sel # acc: %s' % (val_bkd_acc[1], val_bkd_acc[0])
+        # if i > 497:
+        #     print_flag = True
+        # val_tot_acc, val_bkd_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, TRAIN_ENTRY, error_print = print_flag, train_flag = False) #for detailed error analysis, pass True to error_print
+        # print '\nDev sel acc: %s, sel # acc: %s' % (val_bkd_acc[1], val_bkd_acc[0])
         #print ' Breakdown results: agg #: %s, agg: %s,  sel: %s, cond: %s, sel #: %s, cond #: %s, cond col: %s, cond op: %s, cond val: %s, group #: %s, group: %s, order #: %s, order: %s, order agg: %s, order par: %s'\
         #    % (val_bkd_acc[0], val_bkd_acc[1], val_bkd_acc[2], val_bkd_acc[3], val_bkd_acc[4], val_bkd_acc[5], val_bkd_acc[6], val_bkd_acc[7], val_bkd_acc[8], val_bkd_acc[9], val_bkd_acc[10], val_bkd_acc[11], val_bkd_acc[12], val_bkd_acc[13], val_bkd_acc[14])
 
